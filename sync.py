e0bd515765055932d9ee0c8e5ac6f45d62087fee
teresiawairimu <teresiawairimu06.gmail.com>
2025-08-27 12:07:53 -0400

    first milestone

--- sync.py
+++ sync.py
@@ -1,22 +1,48 @@
 import argparse
 import os
 import tempfile
+import sys
 
-def valid_directory(path):
+INIT_FAILURE = 2
+NONFATAL_FAILURE = 1
+SUCCESS = 0
+
+def valid_directory(path: str) -> str:
   if not os.path.isdir(path):
     raise argparse.ArgumentTypeError(f"{path} is not a valid directory")
   return os.path.abspath(path)
 
+def parse_args() -> argparse.Namespace:
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--src", required=True, type=valid_directory, help="path to the source directory")
+  parser.add_argument("--dst"), required=True, type=valid_directory, help="path to the destination directory")
+  return parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dirs", nargs=2, type=valid_directory, help="path to the source and destination directories")
-args = parser.parse_args()
+def init_checks(args: argparse.Namespace) -> None:
+  if args.src == args.dst:
+    print("error: --src and --dst must be different", file=sys.stderr)
+    sys.exit(INIT_FAILURE)
 
-if args.dirs[0] == args.dirs[1]:
-  parser.error("The source and destination directory paths must be different")
+  if not os.access(args.src, os.R_OK | os.X_OK):
+    print(f"error: source directory not readable: {args.src}", file=sys.stderr)
+    sys.exit(INIT_FAILURE)
 
-try:
-  with tempfile.TemporaryFile(dir=args.dirs[1]):
-    pass
-except(OSError, PermissionError):
-  parser.error("The destionation directory is not writable")
+  try:
+    with tempfile.TemporaryFile(dir=args.dst):
+      pass
+  except (OSError, PermissionError):
+    print(f"error: destination directory is not writable: {args.dst}", file=sys.stderr)
+    sys.exit(INIT_FAILURE)
+
+  
+def main() -> None:
+  try:
+    args = parse_args()
+  except SystemExit as e:
+    raise
+  init_checks(args)
+  sys.exit(SUCCESS)
+
+if __name__ == "__main__":
+  main()
+
